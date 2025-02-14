fa77bf4714b13c9b534164551f8e76bdb3c7340e
Dhanish kumar <dhanishmca1@gmail.com>
2025-02-14 12:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let total = [...document.querySelectorAll("table td:nth-child(2)")]
+        .reduce((sum, cell) => sum + (parseFloat(cell.textContent) || 0), 0);
+    
+    let table = document.querySelector("table");
+    let totalRow = document.createElement("tr");
+    totalRow.innerHTML = `<td colspan='2'>Total Price: Rs ${total.toFixed(2)}</td>`;
+    
+    document.querySelector("table tr.total-row")?.remove();
+    totalRow.classList.add("total-row");
+    table.appendChild(totalRow);
   
 };
 
