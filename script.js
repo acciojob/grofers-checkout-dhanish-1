ec66ae5e37aca999196a2ae1987f8994d87dc9f0
Dhanish kumar <dhanishmca1@gmail.com>
2025-02-14 12:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-let total = [...document.querySelectorAll("table td:nth-child(2)")]
-        .reduce((sum, cell) => sum + (+cell.textContent || 0), 0);
+ let total = [...document.querySelectorAll("table td:nth-child(2)")]
+        .reduce((sum, cell) => sum + (+cell.textContent.trim() || 0), 0);
+    
     document.querySelector("table tr.total-row")?.remove();
-    document.querySelector("table").insertAdjacentHTML("beforeend", `<tr class='total-row'><td colspan='2'>Total: Rs ${total}</td></tr>`);
+    
+    let totalRow = document.createElement("tr");
+    totalRow.classList.add("total-row");
+    let totalCell = document.createElement("td");
+    totalCell.colSpan = 2;
+    totalCell.id = "ans";
+    totalCell.textContent = `Total: Rs ${total}`;
+    totalRow.appendChild(totalCell);
+    
+    document.querySelector("table").appendChild(totalRow);
 
   
 };
